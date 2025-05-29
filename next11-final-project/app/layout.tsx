2a33d3eb67d0802937ad4e411c9c760621ad5205
Tri Ngo <tringo831@gmail.com>
2025-05-29 15:23:07 -0700

    Add final project

--- next11-final-project/app/layout.tsx
+++ next11-final-project/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Navbar from "./components/Navbar";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -25,9 +26,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased dark:bg-slate-800`}
       >
-        {children}
+        <Navbar />
+        <main className="px-4 md:px-6 prose prose-xl prose-slate dark:prose-invert mx-auto">
+          {children}
+        </main>
       </body>
     </html>
   );
